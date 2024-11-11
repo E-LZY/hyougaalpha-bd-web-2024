5d119c70bfbfd16bb20872faa9a6b3ea0c5366f6
Pracharawat <54216606+Kanosume@users.noreply.github.com>
2024-11-11 16:50:45 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,53 +56,60 @@ export default function Page() {
 
   const banners = [
     {
+      id: "lew279c3-351e-46a2-b20b-77315dcfade0",
+      name: "hyouga-hbd",
+      url: "",
+      imgURL: "/img/banner/web-banner.png",
+      order: 1
+    },
+    {
       id: "dee279c3-351e-46a2-b20b-77315dcfade0",
       name: "hyouga-keychain",
       url: "https://shop.realic.net/products/2024-november-birthday-event?variant=49287100432664",
       imgURL: "/img/banner/birthday-good.png",
-      order: 1
+      order: 2
     },
     {
       id: "dee279c3-351e-46a2-b20b-77315dcfade0",
       name: "hyouvember",
       url: "https://x.com/search?q=%23%E0%B9%80%E0%B8%AE%E0%B8%B5%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%A7%E0%B8%A1%E0%B9%80%E0%B8%9A%E0%B8%AD%E0%B8%A3%E0%B9%8C&src=typeahead_click",
       imgURL: "/img/banner/hyouvember.jpg",
-      order: 2
+      order: 3
     },
     {
       id: "aa90961a-cfb0-422c-b7ea-50474a71235e",
       name: "cafe-project",
       url: "https://x.com/CNubdao12251",
       imgURL: "/img/banner/cafe.png",
-      order: 3
+      order: 4
     },
     {
       id: "5fae0719-5365-4baa-bc3e-4a8cbe5cb7e4",
       name: "blooming-cat",
       url: "https://x.com/BloomingCat__",
       imgURL: "/img/banner/BloomingCat_Banner.png",
-      order: 4
+      order: 5
     },
     {
       id: "6f82c72d-4598-4de3-9f45-1cdb83f65892",
       name: "game",
       url: "https://play.unity.com/en/games/742ac3a9-3679-4a94-8327-6ccf7018986f/tape-the-cats",
       imgURL: "/img/banner/TapeTheCats.png",
-      order: 5
+      order: 6
     },
     {
       id: "6f82c72d-4598-4de3-9f45-1cda23f65892",
       name: "chaokuy",
       url: "https://x.com/hagaalphyou2211",
       imgURL: "/img/banner/CK-banner.png",
-      order: 6
+      order: 7
     },
     {
       id: "8d3ab3b5-769d-4ab5-b06e-003bf8f2ad3a",
       name: "unknown",
       url: "",
       imgURL: "/img/banner/Unknown.png",
-      order: 7
+      order: 8
     }
   ]
 
